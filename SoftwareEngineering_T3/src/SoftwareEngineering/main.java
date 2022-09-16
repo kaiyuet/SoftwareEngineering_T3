f6317e09fe37513bb8cafaf5cf42469109968db8
kaiyuet <kaiyman3-c@my.cityu.edu.hk>
2022-09-16 09:05:05 +0800

    Create print message

--- SoftwareEngineering_T3/src/SoftwareEngineering/main.java
+++ SoftwareEngineering_T3/src/SoftwareEngineering/main.java
@@ -2,6 +2,6 @@ package SoftwareEngineering;
 
 public class main {
 	public static void Main(String args[]) {
-		
+		System.out.println("Good morning, this is the test file for softwareEngineeringT3");
 	}
 }
